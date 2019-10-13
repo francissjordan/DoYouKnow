ca55fc0767b0b84646c0d06c694dd47d96968eb1
Francisco Jordão <franciscojordas@outlook.com>
2019-10-13 17:11:38 +0200

    importing project

---
+++ DoYouKnow/DoYouKnow/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using DoYouKnow.Models;
+
+namespace DoYouKnow.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
